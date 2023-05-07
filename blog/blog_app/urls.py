42d2cec3455d63f01427da959afd6866aff9aa9a
kubix <maks05102000@gmail.com>
2023-05-07 18:13:15 +0200

    Created Add post view and Update post with urls to them, edited post list template and detail template

--- blog/blog_app/urls.py
+++ blog/blog_app/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
-    path('<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('add-post/', views.PostCreateView.as_view(), name='post_create'),
+    path('post/<int:pk>/edit', views.PostUpdateView.as_view(), name='post_update'),
+    
 ]
--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -1,6 +1,8 @@
+from django.urls import reverse_lazy, reverse
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import (ListView, DetailView, CreateView,
+                                  UpdateView)
 
 
 class PostListView(ListView):
@@ -15,4 +17,20 @@ class PostDetailView(DetailView):
     context_object_name = 'post'
     template_name = 'post_detail.html'
 
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'post_create.html'
+    fields = ['title', 'description']
+    success_url = reverse_lazy('post_list')
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_update.html'
+    fields = ['title', 'description']
+    
+    def get_success_url(self):
+        pk = self.kwargs["pk"]
+        return reverse('post_detail', kwargs={"pk": pk})
 
