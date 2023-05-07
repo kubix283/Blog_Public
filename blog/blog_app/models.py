638b235631dc8171e545836553cccdb15aebffd5
kubix <maks05102000@gmail.com>
2023-05-07 15:29:17 +0200

    Created model Post

--- blog/blog_app/models.py
+++ blog/blog_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
