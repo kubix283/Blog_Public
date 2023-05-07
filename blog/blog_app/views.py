2b5efb47f171d58d084884d8a5bee899b050fa32
kubix <maks05102000@gmail.com>
2023-05-07 16:09:55 +0200

    Added Post list view and template and register post on admin site

--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Post
+from django.views.generic import ListView
 
-# Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'post_list.html'
+
+
