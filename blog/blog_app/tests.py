cfe0bb7b3cd865fef25558ddbe07efbb0f47e16a
kubix <maks05102000@gmail.com>
2023-05-09 20:28:28 +0200

    added functional tests

--- blog/blog_app/tests.py
+++ blog/blog_app/tests.py
@@ -1,12 +1,13 @@
 import pytest
 from .models import Post
 import datetime
-
+from django.urls import reverse
+from pytest_django.asserts import assertTemplateUsed
 
 @pytest.mark.django_db
 def test_post_model(post):
     assert len(Post.objects.all()) == 1
-    assert Post.objects.get(title='test_tytuł') == post
+    assert Post.objects.filter(title='test_tytuł').exists
 
 
 @pytest.mark.django_db
@@ -36,3 +37,40 @@ def test_post_delete():
     assert len(Post.objects.all()) == 1
     post.delete()
     assert len(Post.objects.all()) == 0
+
+@pytest.mark.django_db
+def test_post_list_view(user_client):
+    response = user_client.get(reverse('post_list'))
+    assert response.status_code == 200
+    assertTemplateUsed(response, 'post_list.html')
+
+@pytest.mark.django_db
+def test_post_detail_view(user_client, post):
+    response = user_client.get(reverse('post_detail', args=[post.pk]))
+    assert response.status_code == 200
+    assertTemplateUsed(response, 'post_detail.html')
+    assert response.context['post'] == post 
+
+
+@pytest.mark.django_db
+def test_post_create_view(user_client):
+    data = {'title': 'title', 'description': 'description', 'date_created': datetime.datetime.now()}
+    response = user_client.post(reverse('post_create'), data=data)
+    assert response.status_code == 302
+    assert Post.objects.filter(title='title').exists()
+
+
+@pytest.mark.django_db
+def test_post_update_view(user_client, post):
+    data = {'title': 'new title', 'description': 'new description', 'date_created': datetime.datetime.now()}
+    response = user_client.post(reverse('post_update', args=[post.pk]), data=data)
+    assert response.status_code == 302
+    post.refresh_from_db()
+    assert Post.objects.filter(title='new title').exists()
+
+
+@pytest.mark.django_db
+def test_post_delete_view(user_client, post):
+    response = user_client.post(reverse('post_delete', args=[post.pk]))
+    assert response.status_code == 302
+    assert not Post.objects.filter(pk=post.pk).exists()
--- blog/conftest.py
+++ blog/conftest.py
@@ -1,8 +1,18 @@
 from blog_app.models import Post
+from django.contrib.auth.models import User
+from django.test import Client
 import datetime
 import pytest
 
+
+@pytest.fixture
+def user_client():
+    user = User.objects.create_user(username='testuser', password='password')
+    client = Client()
+    client.login(username='testuser', password='password')
+    return client
+
 @pytest.fixture
 def post():
-    post = Post.objects.create(title='test_tytuł', description='jakis opis', date_created=datetime.datetime.now())
-    return post
+    return Post.objects.create(title='test_tytuł', description='jakis opis', date_created=datetime.datetime.now())
+    
