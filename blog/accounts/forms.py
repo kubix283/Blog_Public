a5958e1da47644f8bbf3447d288c3e78bb675079
kubix <maks05102000@gmail.com>
2023-05-10 21:48:32 +0200

    create a registration, login and logout page and edited a homepage

--- blog/accounts/forms.py
+++ blog/accounts/forms.py
@@ -1,10 +1,20 @@
 from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
-class RegistrationUserForm(forms.ModelForm):
-  
+class RegistrationUserForm(UserCreationForm):
+    email = forms.EmailField(help_text="Enter your email address", required=True)
+
     class Meta:
-        model = User
-        fields = ('username', 'email', 'password',)
+        model = get_user_model()
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super(RegistrationUserForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        
+        return user
+    
--- blog/accounts/views.py
+++ blog/accounts/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
-from django.views.generic import View
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib import messages
+from .forms import RegistrationUserForm
 
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('/')   
+    if request.method == 'POST':
+        form = RegistrationUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, f'Registration successful {user.username}')
+            return redirect('/')   
+        else:
+            for error in list(form.errors.values()):
+                messages.error(request, error)
+    else:
+        form = RegistrationUserForm()
+    
+    return render(request, 'accounts/register.html', {'form': form})
+
+
+def logout_user(request):
+    messages.info(request, f'Logout {request.user.username}')
+    logout(request)
+    return redirect('/')
 
 
