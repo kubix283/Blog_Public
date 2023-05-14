da5980772de2872109c043e34a5bf5e1d01d4d05
kubix <maks05102000@gmail.com>
2023-05-14 18:49:00 +0200

    edited a comment model added author and comment to post

--- blog/comments/models.py
+++ blog/comments/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from blog_app.models import Post
+from accounts.models import CustomUser
 
 class Comment(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     created_at = models.DateField(auto_now_add=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post_comment")
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="author_comment")
 
     def __str__(self):
         return self.title
