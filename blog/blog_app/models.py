abc174f7bfce124b5e6043abb9bb0d6c9b211de6
kubix <maks05102000@gmail.com>
2023-05-14 19:00:36 +0200

    created model comment in blog_app

--- blog/blog_app/models.py
+++ blog/blog_app/models.py
@@ -11,3 +11,14 @@ class Post(models.Model):
     def __str__(self):
         first_title = self.title.split(' ')
         return ' '.join(first_title[:3])
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='author_comment')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_comment')
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
